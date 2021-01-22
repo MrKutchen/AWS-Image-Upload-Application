115a5de057c948bc21e0423294c82d8b62000ad8
Peter <pgkutc@gmail.com>
2021-01-22 18:35:55 -0500

    added react-dropzone for image uploading

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, {useCallback, useEffect, useState} from "react";
 import './App.css';
+import axios from "axios";
+import {useDropzone} from 'react-dropzone'
+
+const UserProfiles = () => {
+
+    const [userProfiles, setUserProfiles] = useState([]);
+
+    const fetchUserProfiles = () => {
+        axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
+            console.log(res);
+            setUserProfiles(res.data);
+        });
+    }
+
+    useEffect(() => {
+        fetchUserProfiles();
+    }, []);
+
+    return userProfiles.map((userProfile, index) => {
+        return (
+            <div key={index}>
+                {/* todo: profile image */}
+                <br/>
+                <br/>
+                <h1>{userProfile.username}</h1>
+                <p>{userProfile.userProfileId}</p>
+                <Dropzone />
+                <br/>
+            </div>
+        );
+    });
+};
+
+function Dropzone() {
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0];
+        console.log(file);
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+
+                    <p>Drop the image here ...</p>
+                ) : (
+                <p>Drag 'n' drop profile image, or click to select profile image</p>
+                )}
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App">
+        <UserProfiles/>
+    </div>;
 }
 
 export default App;
